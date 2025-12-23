1406bf6fdfeedaaa61680609b9259d23877af7ff
yahohulia <yaroslavhohulia@gmail.com>
2025-12-23 14:30:31 +0200

    changed some bugs

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -4,15 +4,3 @@ import { client } from '../utils/fetchClient';
 export const getPosts = (userId: number) => {
   return client.get<Post[]>(`/posts?userId=${userId}`);
 };
-
-export const addPost = ({ userId, title, body }: Omit<Post, 'id'>) => {
-  return client.post<Post>(`/posts`, {
-    userId,
-    title,
-    body,
-  });
-};
-
-export const deletePost = (postId: number) => {
-  return client.delete(`/posts/${postId}`);
-};
